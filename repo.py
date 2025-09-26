09e4f5f77fd9c5c901bcf86640a0f38479c4f6d3
Arkbutan <petroderg@gmail.com>
2025-09-26 14:12:05 +0300

    edited README.md; added add_reader() and remove_reade()

--- repo.py
+++ repo.py
@@ -12,6 +12,16 @@ def remove_book(connect, title, author):
     connect.commit()
     
 def add_reader(connect, full_name, phone, age):
-    pass
+    cursor = connect.cursor()
+    name = full_name.split()[0]
+    surname = full_name.split()[1]
+    pr = f"{name[0]}{surname[0]}{len(name)}{len(surname)}{phone[-4:]}"
+    cursor.execute(f"""INSERT INTO readers (pr, full_name, phone, age)
+                   VALUES ({pr}, {full_name}, {phone}, {age})""")    
+    connect.commit()
+    
 def remove_reader(connect, pr):
-    pass
+    cursor = connect.cursor()
+    cursor.execute(f"""DELETE FROM readers WHERE pr = {pr}""")
+    connect.commit()
+    
