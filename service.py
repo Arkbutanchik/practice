9cb893162ce432cd09629c8c21a99e47cacfc44d
EOGORORORO <e>
2025-09-26 14:21:20 +0300

    service.py start

--- service.py
+++ service.py
@@ -1,7 +1,15 @@
-def book_book():
-
-def remove_book():
-
-def take_book():
-
+def book_book(connect, pr, title, author):
+    pass
+def remove_book(connect, pr, title, author):
+    pass
+def take_book(connect, pr, title, author):
+    pass
+def give_back(connect, pr, title, author):
+    pass
+def see_taken(connect, pr):
+    pass
+def see_booked(connect, pr):
+    pass
+def expired(connect):
+    pass
 def 
